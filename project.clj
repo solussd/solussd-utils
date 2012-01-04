cc6577016b546d815ca04fe84e08bf42bbd0a3dd
Joseph Smith <joe@uwcreations.com>
2012-01-04 16:22:20 -0600

    added trim-chars

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject solussd-utils "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "Useful bits of code"
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
