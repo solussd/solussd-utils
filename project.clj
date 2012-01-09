2004fd98c35ad969ba0b7c7b5e0bf902995ff0aa
Joseph Smith <joe@uwcreations.com>
2012-01-09 17:17:49 -0600

    v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject solussd-utils "1.0.0-SNAPSHOT"
+(defproject solussd-utils "0.1.0"
   :description "Useful bits of code"
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]])
+  :dev-dependencies [[lein-clojars "0.6.0"]
+                     [swank-clojure "1.3.4"]])
--- test/solussd_utils/test/core.clj
+++ test/solussd_utils/test/core.clj
@@ -1,6 +1,3 @@
 (ns solussd-utils.test.core
   (:use [solussd-utils.core])
   (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
