50fd0c6f39a53df331ec26ed5b53dfb93353deb3
Joseph Smith <joe@uwcreations.com>
2012-05-09 13:02:37 -0500

    added sub-if

--- src/solussd_utils/core.clj
+++ src/solussd_utils/core.clj
@@ -1,1 +1,9 @@
 (ns solussd-utils.core)
+
+(defmacro sub-if
+  "If (pred expr) returns true, returns subexpr, otherwise expr"
+  [expr pred subexpr]
+  `(let [expr# ~expr]
+     (if (~pred expr#)
+       ~subexpr
+       expr#)))
