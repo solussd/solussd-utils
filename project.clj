ee6420c8dc024ad60f5e9cb5b21f409b770c1054
Joseph Smith <joe@uwcreations.com>
2012-05-10 13:42:51 -0500

    0.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject solussd-utils "0.2.1"
+(defproject solussd-utils "0.2.2-SNAPSHOT"
   :description "Useful bits of code"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]
